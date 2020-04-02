f68e66acf4dddc55905593afe0b8a578fe240732
oereo <dlstpgns0406@gmail.com>
2020-04-03 03:32:53 +0900

    restframework

--- backend/django/post/views.py
+++ backend/django/post/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework import viewsets, serializers
+from .models import Post, Album, Files
+from .serializers import PostSerializer, AlbumSerializer, FilesSerializer
+from rest_framework.filters import SearchFilter
+from rest_framework.parsers import MultiPartParser, FormParser
 
-# Create your views here.
+
+from rest_framework.response import Response
+from rest_framework import status
+#두가지의 임의의 변수로
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    filter_backends = [SearchFilter]
+    search_fields = ('title', 'content') #튜플형식
+
+
+    def perform_create(self, serializer):
+        serializer.save(author = self.request.user)
+
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        if self.request.user.is_authenticated:
+            qs = qs.filter(author = self.request.user)
+        else:
+            qs = qs.none()
+        return qs
+
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+
+
+class FilesViewSet(viewsets.ModelViewSet):
+    queryset = Files.objects.all()
+    serializer_class = FilesSerializer
+
+    parser_classes = (MultiPartParser, FormParser) #다양한 미디어파일 수락 PARSER CLASS
+
+    def post(self, request, *args, **kwargs):
+        serialzer = FileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(serializer.error, status=HTTP_400_BAD_REQUEST)
